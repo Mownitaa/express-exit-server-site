96ec9db7999aa89e36c8b92b2a1e47261a9f65b4
Mabia Zaman Mownita <mabiamownita@gmail.com>
2023-04-02 20:14:56 +0600

    password changed

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ async function run() {
 
         // GET API
         app.get('/services', async (req, res) => {
+            //
+//             res.setHeader("Access-Control-Allow-Origin", "*")
+// res.setHeader("Access-Control-Allow-Credentials", "true");
+// res.setHeader("Access-Control-Max-Age", "1800");
+// res.setHeader("Access-Control-Allow-Headers", "content-type");
+// res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
+//
             const cursor = servicesCollection.find({});
             const services = await cursor.toArray();
             res.send(services);
